b44f023b2566a5beebbf0ae4491fd4ea7015425b
umar <umartung@gmail.com>
2020-08-28 19:41:50 -0700

    finish project

--- server.js
+++ server.js
@@ -24,9 +24,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.ip, language: req.acceptsLanguages(), software: req.headers['user-agent']});
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
